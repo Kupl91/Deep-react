95c7613a1e017eca9e14cb9724139c765487adbd
Kupl91 <125669838+Kupl91@users.noreply.github.com>
2024-12-01 21:09:01 +0300

    modal

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,25 @@
-import React, { Suspense, useEffect } from 'react';
+import React, { Suspense, useState } from 'react';
 import './styles/index.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
+import { Modal } from 'shared/ui/Modal/Modal';
 
 function App() {
     const { theme } = useTheme();
+
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="">
                 <Navbar />
+                <button onClick={() => setIsOpen(true)}>Open modal</button>
+                <Modal isOpen={isOpen} onClose={() => setIsOpen(false)} >
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta perspiciatis nisi voluptatem sit optio tenetur. Suscipit sed incidunt, illo ea ex officia rem asperiores atque eaque ut esse aspernatur! Quaerat?
+                </Modal>
                 <div className="content-page">
                     <Sidebar />
                     <AppRouter />
---
+++ src/shared/ui/Modal/Modal.tsx
@@ -0,0 +1,45 @@
+import { ReactNode } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './Modal.module.scss';
+
+interface ModalProps {
+    className?: string;
+    children?: ReactNode;
+    isOpen?: boolean;
+    onClose?: () => void;
+}
+
+export const Modal = (props: ModalProps) => {
+    const {
+        className,
+        children,
+        isOpen,
+        onClose,
+    } = props;
+
+    const closeHandler = () => {
+        if (onClose) {
+            onClose();
+        }
+    };
+
+    const onContentClick = (e: React.MouseEvent) => {
+        e.stopPropagation(); // Исправлено: вызов метода
+    };
+
+    const mods: Record<string, boolean> = {
+        [cls.opened]: isOpen,
+    };
+
+    return (
+        <div className={classNames(cls.Modal, mods, [className])}>
+            <div className={cls.overlay} onClick={closeHandler}>
+                <div 
+                 className={cls.content}
+                 onClick={onContentClick}>
+                    {children}
+                </div>
+            </div>
+        </div>
+    );
+};
