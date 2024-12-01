14975b1ec701cb7b0f1a52a47f04e3fc708c29da
Kupl91 <125669838+Kupl91@users.noreply.github.com>
2024-12-01 21:43:09 +0300

    modal closing

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,6 +1,8 @@
-import { ReactNode } from 'react';
+import { ReactNode, useCallback, useEffect, useRef, useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './Modal.module.scss';
+import { on } from 'events';
+import { use } from 'i18next';
 
 interface ModalProps {
     className?: string;
@@ -8,6 +10,8 @@ interface ModalProps {
     isOpen?: boolean;
     onClose?: () => void;
 }
+
+const ANIMATION_DELAY = 300;
 
 export const Modal = (props: ModalProps) => {
     const {
@@ -17,18 +21,42 @@ export const Modal = (props: ModalProps) => {
         onClose,
     } = props;
 
-    const closeHandler = () => {
+    const [isClosing, setIsClosing] = useState(false);
+    const timerRef = useRef<ReturnType<typeof setTimeout>>();
+
+    const closeHandler = useCallback(() => {
         if (onClose) {
-            onClose();
+            setIsClosing(true);
+            timerRef.current = setTimeout(() => {
+                onClose();
+                setIsClosing(false);
+            }, ANIMATION_DELAY);
         }
-    };
+    }, [onClose]);
+
+    const onKeyDown = useCallback((e: KeyboardEvent) => {
+        if (e.key === 'Escape') {
+            closeHandler();
+        }
+    }, [closeHandler])
 
     const onContentClick = (e: React.MouseEvent) => {
-        e.stopPropagation(); // Исправлено: вызов метода
+        e.stopPropagation(); 
     };
+
+    useEffect(() =>{
+        if (isOpen) {
+            window.addEventListener('keydown', onKeyDown);
+        }
+        return () => {
+            clearTimeout(timerRef.current);
+            window.removeEventListener('keydown', onKeyDown);
+        }
+    }, [isOpen , onKeyDown]);
 
     const mods: Record<string, boolean> = {
         [cls.opened]: isOpen,
+        [cls.closing]: isClosing,
     };
 
     return (
